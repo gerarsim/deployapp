3478d9194c09d6ccb642257ee4c7c361bc042b9a
gerarsim <75917703+gerarsim@users.noreply.github.com>
2022-01-23 14:14:41 -0500

    fix

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ module "vpc" {
   version        = "2.38.0"
   name           = "test_ecs_provisioning"
   cidr           = "10.0.0.0/16"
-  azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs            = ["ca-central-1a", "ca-central-1b", "ca-central-1d"]
   public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   tags = {
     "env"       = "dev"
